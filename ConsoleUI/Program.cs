3a51de5cca05741fed2100ca67c1d3d6d0760fb2
Ali Emre Gök <81609259+aliemregk@users.noreply.github.com>
2022-05-19 15:41:31 +0300

    Create project basis

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,28 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new InMemoryProductDal());
+
+            foreach (var item in productManager.GetAll())
+            {
+                Console.WriteLine(item.ProductName);
+            }
+
+            Product product = new Product { ProductId = 6, CategoryId = 2, ProductName = "Test 6", UnitPrice = 30, UnitsInStock = 20 };
+            productManager.Add(product);
+
+            foreach (var item in productManager.GetAll())
+            {
+                Console.WriteLine(item.ProductName);
+            }
+        }
+    }
+}
