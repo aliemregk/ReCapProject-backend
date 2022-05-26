5ca51c264702dc0f73fda217f8954a7fb601a292
Ali Emre Gök <81609259+aliemregk@users.noreply.github.com>
2022-05-26 22:32:55 +0300

    Add Web API and Controllers

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -47,10 +47,10 @@ namespace Business.Concrete
 
         public IDataResult<List<Car>> GetAll()
         {
-            if (DateTime.Now.Hour == 21)
-            {
-                return new ErrorDataResult<List<Car>>(Messages.InvalidList);
-            }
+            //if (DateTime.Now.Hour == 21)
+            //{
+            //    return new ErrorDataResult<List<Car>>(Messages.InvalidList);
+            //}
             return new SuccessDataResult<List<Car>>(_carDal.GetAll(), Messages.Listed);
         }
 
@@ -66,15 +66,11 @@ namespace Business.Concrete
 
         public IDataResult<List<CarDetailDto>> GetCarDetails()
         {
-            if (DateTime.Now.Year == 2022)
-            {
-                return new ErrorDataResult<List<CarDetailDto>>(Messages.InvalidList);
-            }
-            else
-            {
-                return new SuccessDataResult<List<CarDetailDto>>(_carDal.GetCarDetails(), Messages.Listed);
-            }
-
+            //if (DateTime.Now.Year == 2022)
+            //{
+            //    return new ErrorDataResult<List<CarDetailDto>>(Messages.InvalidList);
+            //}
+            return new SuccessDataResult<List<CarDetailDto>>(_carDal.GetCarDetails(), Messages.Listed);
         }
 
         public IResult Update(Car car)
